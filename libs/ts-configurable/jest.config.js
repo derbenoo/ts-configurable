04b7c2ad0260b959c9a2ceec70faba3c6b1ce216
Benjamin Assadsolimani <benjamin.assadsolimani@gmail.com>
2019-03-23 18:57:51 +0100

    Enforce 100% code coverage

--- libs/ts-configurable/jest.config.js
+++ libs/ts-configurable/jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   name: 'ts-configurable',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/ts-configurable',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
