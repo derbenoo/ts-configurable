7a948dcee5ef5f5531e2a482f5c622e0c024b286
Benjamin Assadsolimani <benjamin.assadsolimani@gmail.com>
2019-03-24 19:07:07 +0100

    Instantiate the config class manually to use it for calling the forRoot method of the app module

--- apps/example-nestjs/src/main.ts
+++ apps/example-nestjs/src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from './app/config/config.service';
-import { LoggerService } from './app/logger.service';
+import { LoggerService } from './app/logging/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {});
+  // Instantiate config service
+  const config = new ConfigService();
 
-  const config: ConfigService = app.get(ConfigService);
+  const app = await NestFactory.create(AppModule.forRoot(config), {});
+
+  // Set Nest logger to logging service
   const logger: LoggerService = app.get(LoggerService);
   app.useLogger(logger);
 
+  // Start listening
   await app.listenAsync(config.web.port, config.web.host);
   logger.log(`Webserver listening at ${config.web.url}`);
 }
